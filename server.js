261893c96c4ba887d5811a53b83f9ebc72c503f8
Christopher Smith <christophersmith@ChristophersMBP.bisonnet.lipscomb.edu>
2019-02-09 14:36:51 -0600

    corrected issue with the server code

--- server.js
+++ server.js
@@ -1,12 +1,14 @@
 /* eslint-disable no-console */
 var express = require('express');
-var serveStatic = require('serve-static');
 var cors = require('cors');
 
 var app = express();
-app.use(serveStatic(__dirname + "/dist"));
+app.use(express.static(__dirname + "/dist/"));
 app.use(cors);
 
+app.get(/.*/, function(req,res) {
+    res.sendFile(__dirname + "./dist/index.html")
+});
 app.listen(process.env.PORT || 5000);
 
 console.log('server started ');
